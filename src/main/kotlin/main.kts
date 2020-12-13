c6208084314b4b14085ded67265740118acb8f68
Wout Werkman <wout.werkman@create.nl>
2020-12-13 20:59:03 +0100

    Added time measure

--- src/main/kotlin/main.kts
+++ src/main/kotlin/main.kts
@@ -1,8 +1,14 @@
-print(WebsiteScraper(WebsiteDownloadingRepository())
-    .scrape(Domain("guimp.com"))
-    .flatMap { it.split("\\P{L}+".toRegex()) }
-    .filter { it.isNotEmpty() }
-    .groupBy{ it }
-    .mapValues { (_, words) -> words.size }
-    .entries
-    .sortedWith(compareBy { it.value }))
+import kotlin.system.measureNanoTime
+
+println(
+    measureNanoTime {
+        println(WebsiteScraper(WebsiteDownloadingRepository())
+            .scrape(Domain("guimp.com"))
+            .flatMap { it.split("\\P{L}+".toRegex()) }
+            .filter { it.isNotEmpty() }
+            .groupBy{ it }
+            .mapValues { (_, words) -> words.size }
+            .entries
+            .sortedWith(compareBy { it.value }))
+    } / 1_000_000_000.0
+)
